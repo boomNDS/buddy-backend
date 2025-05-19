2b641f7d31ca341234270e5d3eeae6e81eb23f1f
boomNDS <boomsixboom@gmail.com>
2025-05-19 14:25:28 +0700

    chore: add drizzle-kit in dev

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,15 @@
-import type { Config } from "drizzle-kit"
-import env from "env-var"
+import type { Config } from "drizzle-kit";
+import env from "env-var";
 
-const DATABASE_URL = env.get("DATABASE_URL").required().asString()
+const DATABASE_URL = env.get("DATABASE_URL").required().asString();
 
 export default {
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   casing: "snake_case",
-  dbCredentials: {
-    url: DATABASE_URL
-  }
-} satisfies Config
+  ...(DATABASE_URL && {
+    // only add dbCredentials if URL exists
+    dbCredentials: { url: DATABASE_URL },
+  }),
+} satisfies Config;
